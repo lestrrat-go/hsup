5cd80b8c44219252ffc3373e17620b7e9a5e1f3f
Daisuke Maki <lestrrat+github@gmail.com>
2016-03-01 20:03:46 +0900

    parse flags

--- cmd/hsup/hsup.go
+++ cmd/hsup/hsup.go
@@ -5,17 +5,44 @@ import (
 	"os"
 
 	"github.com/lestrrat/go-hsup"
+	"github.com/jessevdk/go-flags"
 )
 
 func main() {
 	os.Exit(_main())
 }
 
+type options struct {
+	Schema string `short:"s" long:"schema" required:"true" description:"schema file to process"`
+	Flavor string `short:"f" long:"flavor" default:"nethttp" description:"what type of code to generate"`
+}
 func _main() int {
-	b := hsup.NetHTTP
-	if err := b.ProcessFile(os.Args[1]); err != nil {
+	var opts options
+	if _, err := flags.Parse(&opts); err != nil {
+		log.Printf("%s", err)
+		return 1
+	}
+
+	var cb func(options) error
+	switch opts.Flavor {
+	case "nethttp":
+		cb = doNetHTTP
+	default:
+		log.Printf("unknown argument to `flavor`: %s", opts.Flavor)
+		return 1
+	}
+
+	if err := cb(opts); err != nil {
 		log.Printf("%s", err)
 		return 1
 	}
 	return 0
+}
+
+func doNetHTTP(opts options) error {
+	b := hsup.NetHTTP
+	if err := b.ProcessFile(opts.Schema); err != nil {
+		return err
+	}
+	return nil
 }
